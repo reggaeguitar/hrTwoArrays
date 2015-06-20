3c8611ce954d5cce271c5ffa3f932e4c5991a6de
Joseph Brady <escalade2005@hotmail.com>
2015-06-20 15:46:55 -0500

    Initial commit

---
+++ hrTwoArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace hrTwoArrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numCases = Int32.Parse(Console.ReadLine());
+            while (numCases-- > 0)
+            {
+                var nAndK = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+                var numInts = nAndK[0];
+                var k = nAndK[1];
+                var a = Console.ReadLine().Split(' ').Select(Int32.Parse).OrderBy(x => x).ToArray();
+                var b = Console.ReadLine().Split(' ').Select(Int32.Parse).OrderByDescending(x => x).ToArray();
+                bool good = true;
+                for (int i = 0; i < numInts; ++i)
+                {
+                    if (a[i] + b[i] < k)
+                    {
+                        good = false;
+                        break;
+                    }
+                }
+                Console.WriteLine(good ? "YES" : "NO");
+            }
+        }
+    }
+}
